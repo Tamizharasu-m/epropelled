ca641bb7677481b0cdb4d1899d7e0d476840eb4c
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-02-07 18:32:47 +0530

    First commit

--- src/component/Homepage/Section2.jsx
+++ src/component/Homepage/Section2.jsx
@@ -5,7 +5,7 @@ export default function () {
     return (
         <div className='section2'>
             <div className='section2-video'>
-                <video autoPlay muted><source src='./image/home image/video.mp4' ></source></video>
+                <video autoPlay muted><source className="video" src='./image/home image/video.mp4' ></source></video>
             </div>
             <div className='bg'>
                 <h2>the future <br></br> of electric <br></br>propulsion</h2>
